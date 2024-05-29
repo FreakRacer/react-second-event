0d94780385bdd6a3fb8a6ae0e76263c8be666853
FreakRacer <bggy@MacBook-Air-kisabup.local>
2024-05-29 21:03:50 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import Homework from "./Homework";
 
+import { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import Lable from "./components/Lable";
+import Todo from "./components/Todo";
 function App() {
+  const [todos, setTodos] = useState([]);
+  const addText = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substr(2, 9),
+        text: userInput,
+        completed: false,
+      };
+      setTodos([...todos, newItem]);
+      console.log(todos);
+    }
+  };
+ 
+  const removeText = (id) => {
+    setTodos([
+      ...todos.filter((item) => {
+        if (item.id === id) {
+          return false;
+        }
+        return true;
+      }),
+    ]);
+  };
+  const toggleText = (id) => {
+    setTodos([
+      ...todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : { ...todo }
+      ),
+    ]);
+  };
+
   return (
-    <div className="container">
-      <Homework/>
-    </div>
+    <div className="App">
+        <header>
+          <Lable lenght={todos.length}/>
+        </header>
+        <main>
+          <div className={todos.length ? "list" : ""}>
+          {todos.map((todo) => {
+            return (
+              <Todo
+                todo={todo}
+                key={todo.id}
+                toggleText={toggleText}
+                removeText={removeText}
+              />
+            );
+          })}
+          </div>
+          
+          <TodoForm addText={addText} />
+        </main>
+      </div>
+
   );
 }
 
